e83c009aa78080aa36fb25767f97692ef056fa01
s83aloys <s83aloys@hotmail.com>
2015-10-15 16:53:11 +0800

    Update FactorialUtil.cs

--- FactorialProject/FactorialUtil.cs
+++ FactorialProject/FactorialUtil.cs
@@ -11,7 +11,7 @@ namespace FactorialProject
         public long GetFactorial(long x)
         {
             if (x == 0 || x==1)
-                return 0;
+                return 1;
             else
             {
                 long xMin1Fact = GetFactorial(x - 1);
